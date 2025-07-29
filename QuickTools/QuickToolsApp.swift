288870af85aaa91b27ff37e707b7fd722bdbe1a0
shakthi.m <shakthi.m@go-mmt.com>
2025-07-29 12:18:11 +0530

    Initial Commit

---
+++ QuickTools/QuickToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickToolsApp.swift
+//  QuickTools
+//
+//  Created by Shakthi M on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
