c94821a8c3269d645adcf33ad378fccb63a2eb90
shadster-devs <shakthi.m@go-mmt.com>
2025-07-29 23:28:06 +0530

    Bump version to 1.2.4

--- QuickTools/Models/AppConstants.swift
+++ QuickTools/Models/AppConstants.swift
@@ -10,7 +10,7 @@ import SwiftUI
 enum AppConstants {
     // MARK: - App Info
     static let appName = "QuickTools"
-    static let appVersion = "1.2.3"
+    static let appVersion = "1.2.4"
     static let appDescription = "Essential developer tools in your menu bar"
     
     // MARK: - UI Constants
